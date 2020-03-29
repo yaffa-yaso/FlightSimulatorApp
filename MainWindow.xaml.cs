1c6c6eb6ff1b645f75928cef06f8a6010ec36f2d
aaronsium <aaronsm132@gmail.com>
2020-03-29 16:04:55 +0300

    main fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,17 +25,6 @@ namespace FlightSimulatorApp
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            MyFlightModel x = new MyFlightModel(new MyTelnetClient());
-            x.connect("127.0.0.1", 5402);
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            MyFlightModel x = new MyFlightModel(new MyTelnetClient());
-            x.disConnect();
-        }
+       
     }
 }
